c7a3c05dfb08336b60ea5e56d59688e3a6f7ac1e
Emily Pillmore <emily.pillmore@gmail.com>
2016-11-15 20:58:27 -0500

    Added rejoins and removed secret

--- orwell_handler.py
+++ orwell_handler.py
@@ -6,7 +6,7 @@ channel = "#wetfish"
 server = "irc.wetfish.net"
 nick = "Orwell"
 port = 6697
-password = "f8067b8daf5d1b2467b2d448858794468d451232b3de4a0f6262c2ec733acf24"
+password = ""
 
 def loop():
 
@@ -18,21 +18,26 @@ def loop():
             text = orwell.get_text()
             print(text)
 
-            if "PRIVMSG" in text and channel in text and "hello" in text:
-                orwell.send(channel, bytes("Hello, emilypi :)", 'utf-8'))
+            if "PRIVMSG" in text in text and "please quit" in text and "emilypi" in text:
+                orwell.disconnect()
+
+            if "KICK" in text and channel in text and "Orwell" in text:
+                time.sleep(5)
+                orwell.msg("JOIN %s\n" % channel)
+
+
         except socket.error as e:
             if str(e) == "[Errno 35] Resource temporarily unavailable":
                 time.sleep(0)
                 continue
+
             raise e
 
-        finally:
-            orwell.disconnect()
+    orwell.disconnect()
+    return
 
 def main():
-    t = Thread(target=loop)
-    t.daemon = True
-    t.start()
+    loop()
 
 if __name__ == '__main__':
     main()
